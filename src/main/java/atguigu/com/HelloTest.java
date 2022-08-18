8cbaea106703a8175b693f37bd47a4f833bfd35e
sgl <sgl820@email.swu.edu.cn>
2022-08-18 11:35:20 +0800

    first

---
+++ src/main/java/atguigu/com/HelloTest.java
@@ -0,0 +1,11 @@
+package atguigu.com;
+
+/**
+ * @author 苏郭亮
+ * @version 1.0
+ **/
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
