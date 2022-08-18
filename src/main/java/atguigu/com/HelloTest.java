d0ce642e79fa0e1411786e8ca35099da73bfb480
sgl <sgl820@email.swu.edu.cn>
2022-08-18 15:19:31 +0800

    hot-fix first

--- src/main/java/atguigu/com/HelloTest.java
+++ src/main/java/atguigu/com/HelloTest.java
@@ -8,5 +8,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("231");
+        System.out.println("git4");
     }
 }
